001c883fe7fa269f3eaa4fd1ac058d5a066753b8
hongyiz910417 <hongyiz0417@gmail.com>
2015-09-17 00:01:15 -0400

    can scan single file now

--- dataOrganizer.py
+++ dataOrganizer.py
@@ -11,16 +11,44 @@ def round(tm):
 	return rounded
 
 filename = sys.argv[1]
-windowSize = int(sys.argv[2])
-
 f = open(filename, 'rt');
 try:
     reader = csv.reader(f)
-    window = datetime.timedelta(0,windowSize)
+    curDateTime = None
+    count = 0
+    bidSum = 0.0
+    askSum = 0.0
+    bidMax = 0.0
+    bidMin = sys.float_info.max
+    prevBidSum = None
     for row in reader:
-    	res = datetime.datetime.strptime(row[1], "%Y%m%d %H:%M:%S.%f")
-    	#print datetime.datetime.combine(res.date(), datetime.datetime.min.time())
-    	#print res + datetime.timedelta(0,windowSize)
-    	print round(res)
+    	newDateTime = round(datetime.datetime.strptime(row[1], "%Y%m%d %H:%M:%S.%f"))
+    	if(curDateTime == None):
+    		curDateTime = newDateTime
+    	if(newDateTime != curDateTime):
+    		flag = None
+    		if(prevBidSum != None):
+    			flag = prevBidSum < bidSum
+    		prevBidSum = bidSum
+    		print row[0] + "," + curDateTime.strftime("%Y%m%d %H:%M:%S.%f") + "," + str(bidMin) + "," \
+    				+ str(bidMax) + "," + str(bidSum / count) + "," + str((askSum - bidSum) / count) + "," + str(flag)
+    		curDateTime = newDateTime
+    		count = 0
+    		bidSum = 0.0
+    		askSum = 0.0
+    		bidMax = 0.0
+    		bidMin = sys.float_info.max
+    	count += 1
+    	bid = float(row[2])
+    	ask = float(row[3])
+    	bidSum += bid
+    	askSum += ask
+    	if(bid > bidMax):
+    		bidMax = bid
+    	if(bid < bidMin):
+    		bidMin = bid
+    print row[0] + "," + curDateTime.strftime("%Y%m%d %H:%M:%S.%f") + "," + str(bidMin) + "," \
+    				+ str(bidMax) + "," + str(bidSum / count) + "," + str((askSum - bidSum) / count) + "," + str(flag)
 finally:
     f.close()
+
