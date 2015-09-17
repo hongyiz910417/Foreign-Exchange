80f979b4fa5d74fb09a8ba7679bfc97096fc21c8
hongyiz910417 <hongyiz0417@gmail.com>
2015-09-17 15:14:31 -0400

    finished

--- dataOrganizer.py
+++ dataOrganizer.py
@@ -10,9 +10,6 @@ def round(tm):
                              microseconds=tm.microsecond)
 	rounded = tm - discard
 	return rounded
-def getFilenames(dir):
-	names = glob.glob(dir)
-	return sorted(names)
 
 filename = sys.argv[1]
 f = open(filename, 'rt');
@@ -24,35 +21,33 @@ try:
     askSum = 0.0
     bidMax = 0.0
     bidMin = sys.float_info.max
-    prevBidSum = None
+    prevRow = []
     for row in reader:
     	newDateTime = round(datetime.datetime.strptime(row[1], "%Y%m%d %H:%M:%S.%f"))
-    	if(curDateTime == None):
+    	if curDateTime == None:
     		curDateTime = newDateTime
-    	if(newDateTime != curDateTime):
-    		flag = None
-    		if(prevBidSum != None):
-    			flag = prevBidSum < bidSum
-    		prevBidSum = bidSum
-    		print row[0] + "," + curDateTime.strftime("%Y%m%d %H:%M:%S.%f") + "," + str(bidMin) + "," \
-    				+ str(bidMax) + "," + str(bidSum / count) + "," + str((askSum - bidSum) / count) + "," + str(flag)
-    		curDateTime = newDateTime
-    		count = 0
-    		bidSum = 0.0
-    		askSum = 0.0
-    		bidMax = 0.0
-    		bidMin = sys.float_info.max
+    	if newDateTime != curDateTime:
+            flag = None
+            if len(prevRow) != 0:
+                flag = float(prevRow[4]) < (bidSum / count)
+                print prevRow[0] + "," + prevRow[1] + "," + prevRow[2] + "," + prevRow[3] + "," + prevRow[4] + "," + prevRow[5] + "," + str(flag)
+            prevRow = [row[0], curDateTime.strftime("%Y%m%d %H:%M:%S.%f"), str(bidMin), str(bidMax), str(bidSum / count), str((askSum - bidSum) / count)]
+            curDateTime = newDateTime
+            count = 0
+            bidSum = 0.0
+            askSum = 0.0
+            bidMax = 0.0
+            bidMin = sys.float_info.max
     	count += 1
     	bid = float(row[2])
     	ask = float(row[3])
     	bidSum += bid
     	askSum += ask
-    	if(bid > bidMax):
+    	if bid > bidMax:
     		bidMax = bid
-    	if(bid < bidMin):
+    	if bid < bidMin:
     		bidMin = bid
-    print row[0] + "," + curDateTime.strftime("%Y%m%d %H:%M:%S.%f") + "," + str(bidMin) + "," \
-    				+ str(bidMax) + "," + str(bidSum / count) + "," + str((askSum - bidSum) / count) + "," + str(flag)
+    print prevRow[0] + "," + prevRow[1] + "," + prevRow[2] + "," + prevRow[3] + "," + prevRow[4] + "," + prevRow[5] + "," + str(None)
 finally:
     f.close()
 
